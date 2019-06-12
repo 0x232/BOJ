e5bacad7d7030a9540fb1e66f8f47c4330f3597c
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-13 07:36:31 +0900

    Modify 1780 (modify check function)

--- 1780.py
+++ 1780.py
@@ -2,23 +2,24 @@ import sys
 import operator
 
 
-def check(row, col, size, num):
+def check(row, col, size):
     if size == 1:
-        return arr[row][col] == num
+        return True
     for i in range(row, row + size):
         for j in range(col, col + size):
-            if arr[i][j] != num:
+            if arr[i][j] != arr[row][col]:
                 return False
     return True
 
 
 def solve(row, col, size):
-    if check(row, col, size, -1):
-        return (1, 0, 0)
-    elif check(row, col, size, 0):
-        return (0, 1, 0)
-    elif check(row, col, size, 1):
-        return (0, 0, 1)
+    if check(row, col, size):
+        if arr[row][col] == -1:
+            return (1, 0, 0)
+        if arr[row][col] == 0:
+            return (0, 1, 0)
+        if arr[row][col] == 1:
+            return (0, 0, 1)
     else:
         count = (0, 0, 0)
         for i in range(row, row + size, size//3):
