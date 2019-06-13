7aa65edf358d2b4e3425b1612eab6228b19fecdd
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-13 10:38:21 +0900

    Add 5585

---
+++ 5585.py
@@ -0,0 +1,17 @@
+change = 1000 - int(input())
+coin = 0
+while change > 0:
+    if change >= 500:
+        change -= 500
+    elif change >= 100:
+        change -= 100
+    elif change >= 50:
+        change -= 50
+    elif change >= 10:
+        change -= 10
+    elif change >= 5:
+        change -= 5
+    else:
+        change -= 1
+    coin += 1
+print(coin)
