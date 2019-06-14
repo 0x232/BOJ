4c985e2fb19f957ccd90766a9855a32db1418cf1
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-15 02:24:10 +0900

    Modify 1753 (시간 초과 탈출!!)

--- 1753.py
+++ 1753.py
@@ -1,33 +1,26 @@
 import sys
+import heapq
 
 V, E = map(int, sys.stdin.readline().split())
 K = int(sys.stdin.readline())
-
-vertex = list(range(1, V+1))
 edge = {v: [] for v in range(1, V+1)}
 for i in range(E):
     u, v, w = map(int, sys.stdin.readline().split())
     edge[u].append([v, w])
 
-distance = {v: sys.maxsize for v in range(1, V+1)}
+distance = [sys.maxsize]*(V+1)
 distance[K] = 0
+pq = []
+heapq.heappush(pq, (0, K))
 
-while len(vertex) > 0:
-    u = 0
-    d = sys.maxsize
-    for v in vertex:
-        if d > distance[v]:
-            u = v
-            d = distance[v]
-
-    if d == sys.maxsize:
-        break
-
-    vertex.remove(u)
+while pq:
+    d, u = heapq.heappop(pq)
 
     for e in edge[u]:
         v, w = e[0], e[1]
-        distance[v] = min(distance[v], distance[u] + w)
+        if d + w < distance[v]:
+            distance[v] = d + w
+            heapq.heappush(pq, (d + w, v))
 
 for v in range(1, V+1):
     print('INF' if distance[v] == sys.maxsize else distance[v])
