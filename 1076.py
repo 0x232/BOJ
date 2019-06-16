55ea89864592fdd1c114af5f9930ad601655b26a
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-16 20:16:32 +0900

    Add 1076

---
+++ 1076.py
@@ -0,0 +1,6 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow',
+         'green', 'blue', 'violet', 'grey', 'white']
+x = color.index(input())
+y = color.index(input())
+z = color.index(input())
+print((10*x+y)*10**z)
