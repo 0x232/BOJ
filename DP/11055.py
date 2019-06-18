d83b53901614f0993cfa9e906e18b416ab7f23fc
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-18 21:40:57 +0900

    Modify 11055 (add Test Case comment)

--- DP/11055.py
+++ DP/11055.py
@@ -4,5 +4,10 @@ dp = A.copy()
 for i in range(len(A)):
     for j in range(i):
         if A[j] < A[i]:
+            '''
+            Test Case
+            10
+            1 100 55 50 60 3 5 6 7 8
+            '''
             dp[i] = max(dp[i], dp[j]+A[i])
 print(max(dp))
