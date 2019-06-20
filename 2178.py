cb757ea3a2cb35dba50707066d2c6371af014d94
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-21 02:26:05 +0900

    Add 2178 (Flood Fill / BFS)

---
+++ 2178.py
@@ -0,0 +1,32 @@
+import queue
+
+
+def is_in_range(n, m, row, col):
+    return row >= 0 and row < n and col >= 0 and col < m
+
+
+def BFS(n, m, row, col):
+    q = queue.Queue()
+    q.put((row, col))
+    distance[row][col] = 1
+    while not q.empty():
+        r, c = q.get()
+        dr = [-1, 1, 0, 0]
+        dc = [0, 0, -1, 1]
+        for i in range(4):
+            if is_in_range(n, m, r+dr[i], c+dc[i]) and \
+               distance[r+dr[i]][c+dc[i]] == -1 and \
+               maze[r+dr[i]][c+dc[i]] == '1':
+                q.put((r+dr[i], c+dc[i]))
+                distance[r+dr[i]][c+dc[i]] = distance[r][c] + 1
+
+maze = []
+distance = []
+
+N, M = map(int, input().split())
+for _ in range(N):
+    maze.append(input())
+    distance.append([-1] * M)
+
+BFS(N, M, 0, 0)
+print(distance[N-1][M-1])
