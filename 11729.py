c87ddd0dcda0f4cd9ba5e7c27d253c82d23cfbca
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-20 11:02:10 +0900

    Add 11729 and 1914 (Tower of Hanoi)

---
+++ 11729.py
@@ -0,0 +1,18 @@
+def number_of_movement(N):
+    if N == 1:
+        return 1
+    else:
+        return 2*number_of_movement(N-1) + 1
+
+
+def print_movement(A, B, N):
+    if N == 1:
+        print(A, B)
+    else:
+        print_movement(A, 6-A-B, N-1)
+        print(A, B)
+        print_movement(6-A-B, B, N-1)
+
+N = int(input())
+print(number_of_movement(N))
+print_movement(1, 3, N)
