614c723c9349dc572d6451f421bd4be92ec33117
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-21 16:28:02 +0900

    Modify 7576 (시간 초과 탈출!!)

--- 7576.py
+++ 7576.py
@@ -6,15 +6,8 @@ def is_in_range(n, m, row, col):
     return row >= 0 and row < n and col >= 0 and col < m
 
 
-def BFS(n, m, row, col, number_of_ripe_tomato):
-    # Initialization.
-    for i in range(n):
-        for j in range(m):
-            visited[i][j] = False
-
-    q = collections.deque([(row, col)])
-    visited[row][col] = True
-    days_to_ripen[row][col] = 0
+def BFS(n, m, row, col, ripe_tomato, number_of_ripe_tomato):
+    q = collections.deque(ripe_tomato)
     while q:
         r, c = q.popleft()
         for dr, dc in (-1, 0), (1, 0), (0, -1), (0, 1):
@@ -23,11 +16,7 @@ def BFS(n, m, row, col, number_of_ripe_tomato):
                not visited[r+dr][c+dc]:
                 q.append((r+dr, c+dc))
                 visited[r+dr][c+dc] = True
-                if number_of_ripe_tomato > 1:
-                    days_to_ripen[r+dr][c+dc] = \
-                        min(days_to_ripen[r+dr][c+dc], days_to_ripen[r][c]+1)
-                else:
-                    days_to_ripen[r+dr][c+dc] = days_to_ripen[r][c] + 1
+                days_to_ripen[r+dr][c+dc] = days_to_ripen[r][c] + 1
 
 M, N = map(int, sys.stdin.readline().split())
 box, visited, days_to_ripen = [], [], []
@@ -36,12 +25,17 @@ for _ in range(N):
     visited.append([False]*M)
     days_to_ripen.append([-1]*M)
 
+ripe_tomato = []
 number_of_ripe_tomato = 0
 for i in range(N):
     for j in range(M):
         if box[i][j] == '1':
             number_of_ripe_tomato += 1
-            BFS(N, M, i, j, number_of_ripe_tomato)
+            ripe_tomato.append((i, j))
+            visited[i][j] = True
+            days_to_ripen[i][j] = 0
+
+BFS(N, M, i, j, ripe_tomato, number_of_ripe_tomato)
 
 box = [x for l in box for x in l]
 days_to_ripen = [x for l in days_to_ripen for x in l]
