1f9cb62cec30c59d5a3a260062c9f457726447f6
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-22 13:10:50 +0900

    Modify 15953

--- 15953.py
+++ 15953.py
@@ -1,20 +1,23 @@
+import sys
 import math
 
-for _ in range(int(input())):
-    a, b = map(int, input().split())
+for _ in range(int(sys.stdin.readline())):
+    a, b = map(int, sys.stdin.readline().split())
     prize_money = 0
 
-    if a == 1:
+    if a == 0 or a > 21:
+        pass
+    elif a == 1:
         prize_money += 5000000
-    if a > 1 and a < 4:
+    elif a < 4:
         prize_money += 3000000
-    if a > 3 and a < 7:
+    elif a < 7:
         prize_money += 2000000
-    if a > 6 and a < 11:
+    elif a < 11:
         prize_money += 500000
-    if a > 10 and a < 16:
+    elif a < 16:
         prize_money += 300000
-    if a > 15 and a < 22:
+    elif a < 22:
         prize_money += 100000
 
     if b > 0 and b < 32:
