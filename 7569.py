da8f568b918657c15f51bba83e2feabda13ca8b2
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-23 10:08:21 +0900

    Modify 7569 (reduce time by not using is_in_range function)

--- 7569.py
+++ 7569.py
@@ -1,17 +1,9 @@
 import sys
 import collections
 
-
-def is_in_range(H, N, M, h, r, c):
-    return h >= 0 and r >= 0 and c >= 0 and \
-           h < H and r < N and c < M
-
 M, N, H = map(int, sys.stdin.readline().split())
-box = [[] for _ in range(H)]
+box = [[sys.stdin.readline().split() for _ in range(N)] for _ in range(H)]
 visited = [[[False]*M for _ in range(N)] for _ in range(H)]
-for i in range(H):
-    for _ in range(N):
-        box[i].append(sys.stdin.readline().split())
 
 queue = collections.deque([])
 number_of_tomato = M*N*H
@@ -23,19 +15,20 @@ for i in range(H):
             if box[i][j][k] == '-1':
                 number_of_tomato -= 1
 
+direction = [(-1, 0, 0), (1, 0, 0), (0, -1, 0),
+             (0, 1, 0), (0, 0, -1), (0, 0, 1)]
 days_to_ripen = 0
 
 while queue:
     h, r, c, days_to_ripen = queue.popleft()
     number_of_tomato -= 1
-    direction = [(-1, 0, 0), (1, 0, 0), (0, -1, 0),
-                 (0, 1, 0), (0, 0, -1), (0, 0, 1)]
-    for i, j, k in direction:
-        if is_in_range(H, N, M, h+i, r+j, c+k) and \
-           not visited[h+i][r+j][c+k] and \
-           box[h+i][r+j][c+k] == '0':
-            queue.append((h+i, r+j, c+k, days_to_ripen+1))
-            visited[h+i][r+j][c+k] = True
-            box[h+i][r+j][c+k] = '1'
+        for i, j, k in direction:
+        nh, nr, nc = h+i, r+j, c+k
+        if nh >= 0 and nr >= 0 and nc >= 0 and \
+           nh < H and nr < N and nc < M and \
+           not visited[nh][nr][nc] and \
+           box[nh][nr][nc] == '0':
+            queue.append((nh, nr, nc, days_to_ripen+1))
+            visited[nh][nr][nc] = True
 
 print(days_to_ripen if number_of_tomato == 0 else -1)
