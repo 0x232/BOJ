aad9ed2d7e7b6af736e80900de2d9b2f288e16db
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-23 10:46:53 +0900

    Modify 7569 (reduce time)

--- 7569.py
+++ 7569.py
@@ -2,17 +2,20 @@ import sys
 import collections
 
 M, N, H = map(int, sys.stdin.readline().split())
-box = [[sys.stdin.readline().split() for _ in range(N)] for _ in range(H)]
+box = [[['0']*M for _ in range(N)] for _ in range(H)]
 visited = [[[False]*M for _ in range(N)] for _ in range(H)]
 
 queue = collections.deque([])
 number_of_tomato = M*N*H
 for i in range(H):
     for j in range(N):
+        s = sys.stdin.readline().split()
         for k in range(M):
-            if box[i][j][k] == '1':
+            if s[k] == '1':
+                box[i][j][k] = s[k]
                 queue.append((i, j, k, 0))
-            if box[i][j][k] == '-1':
+            if s[k] == '-1':
+                box[i][j][k] = s[k]
                 number_of_tomato -= 1
 
 direction = [(-1, 0, 0), (1, 0, 0), (0, -1, 0),
@@ -22,12 +25,34 @@ days_to_ripen = 0
 while queue:
     h, r, c, days_to_ripen = queue.popleft()
     number_of_tomato -= 1
-        for i, j, k in direction:
-        nh, nr, nc = h+i, r+j, c+k
-        if nh >= 0 and nr >= 0 and nc >= 0 and \
-           nh < H and nr < N and nc < M and \
-           not visited[nh][nr][nc] and \
-           box[nh][nr][nc] == '0':
+    nh, nr, nc = h-1, r, c
+    if nh >= 0:
+        if box[nh][nr][nc] == '0' and not visited[nh][nr][nc]:
+            queue.append((nh, nr, nc, days_to_ripen+1))
+            visited[nh][nr][nc] = True
+    nh, nr, nc = h+1, r, c
+    if nh < H:
+        if box[nh][nr][nc] == '0' and not visited[nh][nr][nc]:
+            queue.append((nh, nr, nc, days_to_ripen+1))
+            visited[nh][nr][nc] = True
+    nh, nr, nc = h, r-1, c
+    if nr >= 0:
+        if box[nh][nr][nc] == '0' and not visited[nh][nr][nc]:
+            queue.append((nh, nr, nc, days_to_ripen+1))
+            visited[nh][nr][nc] = True
+    nh, nr, nc = h, r+1, c
+    if nr < N:
+        if box[nh][nr][nc] == '0' and not visited[nh][nr][nc]:
+            queue.append((nh, nr, nc, days_to_ripen+1))
+            visited[nh][nr][nc] = True
+    nh, nr, nc = h, r, c-1
+    if nc >= 0:
+        if box[nh][nr][nc] == '0' and not visited[nh][nr][nc]:
+            queue.append((nh, nr, nc, days_to_ripen+1))
+            visited[nh][nr][nc] = True
+    nh, nr, nc = h, r, c+1
+    if nc < M:
+        if box[nh][nr][nc] == '0' and not visited[nh][nr][nc]:
             queue.append((nh, nr, nc, days_to_ripen+1))
             visited[nh][nr][nc] = True
 
