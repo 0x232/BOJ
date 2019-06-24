794449cebdc7c50b7a279274fc433ad4e79a2af2
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-24 18:45:52 +0900

    Modify 3040 (use for loop instead of itertools.combinations)

--- 3040.py
+++ 3040.py
@@ -1,7 +1,9 @@
-import itertools
-
 number = [int(input()) for _ in range(9)]
-for x in itertools.combinations(number, 7):
-    if sum(x) == 100:
-        print('\n'.join(map(str, x)))
-        break
+total = sum(number)
+for i in range(9):
+    for j in range(9):
+        if total - number[i] - number[j] == 100:
+            for k in range(9):
+                if k != i and k != j:
+                    print(number[k])
+            break
