69b4a2d233369f799608f7d029c275f55a48c1c4
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-25 03:52:32 +0900

    Modify 1212 (use python built-in functions)

--- 1212.py
+++ 1212.py
@@ -1,13 +1,1 @@
-converter = ['000', '001', '010', '011',
-             '100', '101', '110', '111']
-for i, s in enumerate(input()):
-    n = int(s)
-    if i > 0:
-        print(converter[n], end='')
-    else:
-        if n < 2:
-            print(converter[n][2], end='')
-        elif n < 4:
-            print(converter[n][1:], end='')
-        else:
-            print(converter[n], end='')
+print(bin(int(input(), 8))[2:])
