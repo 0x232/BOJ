26ab46a794c4622cdf7507232e4d39da65946493
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-24 19:10:44 +0900

    Modify 3041 (reduce time)

--- 3041.py
+++ 3041.py
@@ -1,9 +1,9 @@
-puzzle = [list(input()) for _ in range(4)]
 distance = 0
 for i in range(4):
+    puzzle = input()
     for j in range(4):
-        if puzzle[i][j] == '.':
+        if puzzle[j] == '.':
             continue
-        idx = ord(puzzle[i][j])-65
-        distance += abs(i - (idx // 4)) + abs(j - (idx % 4))
+        idx = ord(puzzle[j]) - 65
+        distance += abs(i - idx // 4) + abs(j - idx % 4)
 print(distance)
