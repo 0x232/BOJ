532daece282927309da7a063d4b5cad24bd81a99
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-29 16:54:02 +0900

    Modify 14888

--- 14888.cpp
+++ 14888.cpp
@@ -5,20 +5,21 @@
 using namespace std;
 
 vector<int> numbers;
-vector<char> op;
+vector<int> op;
+int add = 0, subtract = 1, multiply = 2, divide = 3;
 vector<bool> visited;
 int minValue = -1e9, maxValue = 1e9;
 
-void calculate(vector<char>& picked) {
+void calculate(vector<int>& picked) {
 	int result = numbers[0];
 	for (size_t i = 0; i < picked.size(); i++) {
-		if (picked[i] == '+')
+		if (picked[i] == add)
 			result += numbers[i + 1];
-		if (picked[i] == '-')
+		if (picked[i] == subtract)
 			result -= numbers[i + 1];
-		if (picked[i] == '*')
+		if (picked[i] == multiply)
 			result *= numbers[i + 1];
-		if (picked[i] == '/')
+		if (picked[i] == divide)
 			result /= numbers[i + 1];
 	}
 	if (minValue == -1e9)
@@ -29,7 +30,7 @@ void calculate(vector<char>& picked) {
 	maxValue = max(result, maxValue);
 }
 
-void pick(int N, int toPick, vector<char>& picked) {
+void pick(int N, int toPick, vector<int>& picked) {
 	if (toPick == 0) {
 		calculate(picked);
 		return;
@@ -57,14 +58,13 @@ int main() {
 	for (int i = 0; i < 4; i++)
 		cin >> numberOfOperator[i];
 	op.resize(N - 1);
-	vector<char> symbolOfOperator = { '+', '-', '*', '/' };
-	vector<char>::iterator iter = op.begin();
+	vector<int>::iterator iter = op.begin();
 	for (int i = 0; i < 4; i++) {
-		fill(iter, iter + numberOfOperator[i], symbolOfOperator[i]);
+		fill(iter, iter + numberOfOperator[i], i);
 		iter += numberOfOperator[i];
 	}
 	visited.resize(N - 1, false);
-	vector<char> picked;
+	vector<int> picked;
 	pick(N - 1, N - 1, picked);
 	cout << maxValue << '\n' << minValue;
 	return 0;
