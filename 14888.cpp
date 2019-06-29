ff12f5c9fa550b53f8dec4450a4994403fb36332
0x232 <51640066+0x232@users.noreply.github.com>
2019-06-29 18:10:49 +0900

    Modify 14888 (better solution)

--- 14888.cpp
+++ 14888.cpp
@@ -1,49 +1,25 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> numbers;
-vector<int> op;
-int add = 0, subtract = 1, multiply = 2, divide = 3;
-vector<bool> visited;
-int minValue = -1e9, maxValue = 1e9;
+int numbers[11];
+int minValue = 1e9, maxValue = -1e9;
 
-void calculate(vector<int>& picked) {
-	int result = numbers[0];
-	for (size_t i = 0; i < picked.size(); i++) {
-		if (picked[i] == add)
-			result += numbers[i + 1];
-		if (picked[i] == subtract)
-			result -= numbers[i + 1];
-		if (picked[i] == multiply)
-			result *= numbers[i + 1];
-		if (picked[i] == divide)
-			result /= numbers[i + 1];
-	}
-	if (minValue == -1e9)
-		minValue = result;
-	if (maxValue == 1e9)
-		maxValue = result;
-	minValue = min(result, minValue);
-	maxValue = max(result, maxValue);
-}
-
-void pick(int N, int toPick, vector<int>& picked) {
+void pick(int N, int toPick, int a, int s, int m, int d, int result) {
 	if (toPick == 0) {
-		calculate(picked);
+		minValue = min(result, minValue);
+		maxValue = max(result, maxValue);
 		return;
 	}
-	for (int i = 0; i < N; i++) {
-		if (!visited[i]) {
-			visited[i] = true;
-			picked.push_back(op[i]);
-			pick(N, toPick - 1, picked);
-			visited[i] = false;
-			picked.pop_back();
-		}
-	}
+	if (a > 0)
+		pick(N, toPick - 1, a - 1, s, m, d, result + numbers[N - toPick]);
+	if (s > 0)
+		pick(N, toPick - 1, a, s - 1, m, d, result - numbers[N - toPick]);
+	if (m > 0)
+		pick(N, toPick - 1, a, s, m - 1, d, result * numbers[N - toPick]);
+	if (d > 0)
+		pick(N, toPick - 1, a, s, m, d - 1, result / numbers[N - toPick]);
 }
 
 int main() {
@@ -51,21 +27,11 @@ int main() {
 	cin.tie(NULL);
 	int N;
 	cin >> N;
-	numbers.resize(N);
 	for (int i = 0; i < N; i++)
 		cin >> numbers[i];
-	int numberOfOperator[4];
-	for (int i = 0; i < 4; i++)
-		cin >> numberOfOperator[i];
-	op.resize(N - 1);
-	vector<int>::iterator iter = op.begin();
-	for (int i = 0; i < 4; i++) {
-		fill(iter, iter + numberOfOperator[i], i);
-		iter += numberOfOperator[i];
-	}
-	visited.resize(N - 1, false);
-	vector<int> picked;
-	pick(N - 1, N - 1, picked);
+	int a, s, m, d;
+	cin >> a >> s >> m >> d;
+	pick(N, N-1, a, s, m, d, numbers[0]);
 	cout << maxValue << '\n' << minValue;
 	return 0;
 }
